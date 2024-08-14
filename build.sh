054276b285fab420b7db786edfb7b02d2c8417d0
SidGames5 <saytleader@outlook.com>
2024-08-14 08:51:11 -0400

    Fixed the build script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-rm portfolio-roast.tar
+rm -f portfolio-roast.tar
 sudo docker build -t portfolio-roast .
 sudo docker save portfolio-roast:latest -o portfolio-roast.tar
 sudo chmod 777 ./portfolio-roast.tar
